21811debfc2733a5a6a1c548fa2a079763ccfd44
Maks <madmax20@bk.ru>
2025-10-10 22:41:30 +0300

    second_commit

---
+++ deals/urls.py
@@ -0,0 +1,29 @@
+"""
+URL configuration for deals project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+
+from deals.views.deals_views import create_deal_view, success_view, get_deal_list, \
+    get_dashboard, get_deal_view
+
+urlpatterns = [
+    path('', get_dashboard, name='dashboard'),
+    path('create/', create_deal_view, name='create_deal'),
+    path('success/', success_view, name='deal_success'),
+    path('list/', get_deal_list, name='deal_list'),
+
+    path('<int:pk>/', get_deal_view, name='deal_detail'),
+]
